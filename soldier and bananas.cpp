f22d65f79144692f07490d320e8bb3dd7e98b9ca
ravi-rathore-securedev <sirravirathore@gmail.com>
2025-03-10 18:48:25 +0530

    Problem :: soldier and bananas.cpp

---
+++ soldier and bananas.cpp
@@ -0,0 +1,25 @@
+/*
+    ***********************
+    Coder :: sirravirathore
+    ***********************
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+    
+    int k,n,w;
+    cin>>k>>n>>w;
+
+    int ans = k*(w*(w+1)/2)-n;
+    if(ans<0)
+        ans=0;
+    cout<<ans;
+   
+
+    cout<<"\n";
+    return 0;
+}
