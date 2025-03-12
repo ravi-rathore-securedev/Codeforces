a8d66a204094c5501258738ed6d4ae36237635c4
ravi-rathore-securedev <sirravirathore@gmail.com>
2025-03-12 13:08:46 +0530

    Problem :: wrong subtraction.cpp

---
+++ wrong subtraction.cpp
@@ -0,0 +1,27 @@
+/*
+    ***********************
+    Coder :: sirravirathore
+    ***********************
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+    
+   int n,k;
+   cin>>n>>k;
+
+   while(k--){
+    if(n%10!=0)
+        n--;
+    else
+        n=n/10;
+   }
+   cout<<n;
+
+    cout<<"\n";
+    return 0;
+}
