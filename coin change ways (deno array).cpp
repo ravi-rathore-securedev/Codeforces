1078344a5660be88f67ef6ae93041e305de96272
ravi-rathore-securedev <www.ravi3598@gmail.com>
2025-03-23 22:58:52 +0530

    coin change ways (deno array)

---
+++ coin change ways (deno array).cpp
@@ -0,0 +1,43 @@
+/*
+    ***********************
+    Coder :: sirravirathore
+    ***********************
+*/
+/* Problem: Coin Change
+    you are given coins of denominations (1,3,5,10)
+    and you need to make a change the n in these denomination
+    return the total number of ways to make a change of n
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    
+    int n,k;
+    cin>>n>>k;
+    cin.ignore();
+
+    vector<int>coins(k);
+
+    for(int i=0;i<k;i++){
+        cin>>coins[i];
+    }
+
+    vector<int>t(n+1);
+
+    t[0]=1;
+
+    for(int i=1;i<=n;i++){
+        for(auto &coin:coins){
+            if(i-coin>=0){
+                t[i]+=t[i-coin];
+            }
+        }
+    }
+    cout<<t[n];
+    
+    cout<<endl;
+    return 0;
+}
