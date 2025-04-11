fe4e92d654ea8b477ff35df424e2d4df2ea3dd54
ravi-rathore-securedev <www.ravi3598@gmail.com>
2025-04-11 20:00:17 +0530

    problem count number of subset sum  given difference.cpp

---
+++ count number of subset sum  given difference.cpp
@@ -0,0 +1,67 @@
+/*
+    ***********************
+    Coder :: sirravirathore
+    ***********************
+*/
+#include<bits/stdc++.h> 
+using namespace std;
+
+// count number of subset sum  given difference
+
+//Classes
+class knapsack{
+    public:
+        vector<int>arr;
+        int k;
+        int MinSubsetSumDiff(){
+            int n=arr.size();
+            if(n==0)
+                return 0;
+
+            int h=accumulate(arr.begin(),arr.end(),0);
+            int W= (h + k)/2;
+
+            vector<vector<int>>t(n+1,vector<int>(h+1));
+
+            for(int i=0;i<=W;i++){
+                t[0][i]=0;
+            }
+            for(int i=0;i<=n;i++){
+                t[i][0]=1;
+            }
+
+
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=W;j++){
+                    if(arr[i-1]<=j){
+                        t[i][j]=t[i-1][j-arr[i-1]]+t[i-1][j];
+                    }
+                    else{
+                        t[i][j]=t[i-1][j];
+                    }
+                }
+            }
+            return t[n][W];
+           
+        }
+};
+ 
+int main(){
+    
+    knapsack p;
+
+    p.arr={1,4,10,6,8,7,5};
+    p.k=1;
+    cout<<p.MinSubsetSumDiff();
+
+    cout<<endl;
+    return 0;
+}
+
+/*
+10 6 4 1
+10 7 4
+8 7
+5 4
+6 7
+*/
